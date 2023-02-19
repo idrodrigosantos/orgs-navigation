c6d5534df18084b23b802f5692bf4a63288244f7
Rodrigo Santos <rodrigosantos.git@gmail.com>
2023-02-19 11:43:59 -0300

    Add React Navigation

--- App.tsx
+++ App.tsx
@@ -1,28 +1,18 @@
 import { SafeAreaView, StatusBar } from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './src/telas/Home';
-import Cesta from './src/telas/Cesta';
+// import MelhoresProdutores from './src/telas/MelhoresProdutores';
 
 export default function App() {
-  const produtores = useProdutores(false);
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar />
 
-      <Home melhoresProdutores={false} />
-      {/* <Home melhoresProdutores={true} /> */}
-
-      {/* {produtores.length > 0 && (
-        <Cesta
-          produtor={{
-            nome: produtores[0].nome,
-            imagem: produtores[0].imagem,
-          }}
-          {...produtores[0].cestas[0]}
-        />
-      )} */}
+      <NavigationContainer>
+        <Home />
+        {/* <MelhoresProdutores /> */}
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
